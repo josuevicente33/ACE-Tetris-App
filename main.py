1931088c180a09cdc4b80b6ddf2eda7e1bdad3fd
Josue Vicente <josue.vicente@ufl.edu>
2022-10-25 16:04:55 -0400

    PyAutoGUI was added

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pyautogui
+
+# we will use
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
